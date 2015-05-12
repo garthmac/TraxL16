957a9b1cd490d7c79c52c31f76099faf74b9e650
iMac21.5 <imac@iMac215.local>
2015-05-12 15:41:03 -0600

    Initial Commit

---
+++ TraxL16Tests/TraxL16Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TraxL16Tests.swift
+//  TraxL16Tests
+//
+//  Created by iMac21.5 on 5/12/15.
+//  Copyright (c) 2015 Garth MacKenzie. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TraxL16Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
